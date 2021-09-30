69d27554c28e6e7ee308d0e9ef46e6c1ef3d781a
Luis <pereira1214@gmail.com>
2021-09-30 10:11:51 -0400

    commit tree 96b8f0c2087167b068de452876d0002cd74144ae

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(useName,useEmail) {
+    const formData = {
+        name: useName,
+        email: useEmail,
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+     return fetch("http://localhost:3000/users", configurationObject)
+     .then(response => response.json())
+     .then(data => {
+         console.log(data.id.value);
+
+         document.body.innerHTML = data.id;
+     })
+     .catch(function (error) {
+        alert("Error, Will Robinson!!");
+        console.log(error.message);
+        document.body.innerHTML = error.message;
+      });
+}
+
+submitData("Steve", "steve@steve.com");
